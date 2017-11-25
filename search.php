cf5b265e8344d1e409de4c5d9028ba1cf0470bec
arcadio1894 <joryes1894@gmail.com>
2017-11-25 16:58:21 -0500

    cambios en los titulos

--- search.php
+++ search.php
@@ -10,7 +10,7 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-	<title>Buy_shop an E-Commerce online Shopping Category Flat Bootstarp responsive Website Template| Men :: w3layouts</title>
+	<title>Buscar</title>
 
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
