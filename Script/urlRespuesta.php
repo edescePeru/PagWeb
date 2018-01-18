5092ea33057df017be0188ad95d009364b8f9ad4
arcadio1894 <joryes1894@gmail.com>
2018-01-18 17:12:38 -0500

    Create Compra

--- Script/urlRespuesta.php
+++ Script/urlRespuesta.php
@@ -1,4 +1,6 @@
 <?php
+include_once '../BaseDatos/conexion.php';
+
 $ApiKey = "4Vj8eK4rloUd272L48hsrarnUA";
 $merchant_id = $_REQUEST['merchantId'];
 $referenceCode = $_REQUEST['referenceCode'];
@@ -16,7 +18,7 @@ $pseBank = $_REQUEST['pseBank'];
 $lapPaymentMethod = $_REQUEST['lapPaymentMethod'];
 $transactionId = $_REQUEST['transactionId'];
 
-echo $transactionId;
+//echo $transactionId;
 
 if ($_REQUEST['transactionState'] == 4 ) {
 	$estadoTx = "Transacción aprobada";
@@ -40,6 +42,27 @@ else {
 
 
 if (strtoupper($firma) == strtoupper($firmacreada)) {
+	// Obtener la referencia y dr de baja al carrito y guardar la compra
+	$idCarrito = $referenceCode;
+	$query = "UPDATE carrito SET sold = 0";
+	$edicion = mysqli_query($conexion, $query);
+
+	// Guardar la compra 
+	$query_insert="INSERT INTO compra(idCarrito, fechaCreacion, enable) VALUES (".$idCarrito.", '".date('Y-m-d')."', 1)";
+	$result_insert = mysqli_query($conexion, $query_insert);
+	$last_id = mysqli_insert_id($conexion);
+	//echo $last_id.' '.$idProducto.' '.$res[6];
+	$query_select = "SELECT * FROM detacarrito WHERE idCarrito = ".$idCarrito;
+	$result_select = mysqli_query($conexion, $query_select);
+	if (mysqli_num_rows($result_select)>0) {
+		while ($fila = mysqli_fetch_array($result_select)) {
+			$query_insert2 ="INSERT INTO detacompra (idCompra, idProducto, cantidad, precio, enable) 
+			VALUES (".$last_id.", ".$fila[1].", ".$fila[2].", ".$fila[3].", 1)";
+			$result_insert2 = mysqli_query($conexion, $query_insert2);
+		}
+	}
+	
+		
 	header('Location: /../success.php');
 }
 else
